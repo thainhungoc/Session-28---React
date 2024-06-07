90736eeaf61805b174c68746f3710a0aceffca78
Yuamy205 <ngocarmy0710g@gmail.com>
2024-06-07 08:43:17 +0700

    240606

---
+++ vite-project/src/App.tsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+
+import Bai1 from "./ss28/Bai1";
+// import Bai2 from "./ss28/Bai2"
+import Bai3 from "./ss28/Bai3";
+import Bai4 from "./ss28/Bai4";
+
+function App() {
+  return (
+    <>
+      <Bai1/>
+      {/* <Bai2 /> */}
+      <Bai3 />
+      <Bai4 />
+    </>
+  );
+}
+
+export default App;
